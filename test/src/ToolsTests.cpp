335512e8f8e9504eecb8b782fcb21ab300bb519f
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2022-10-23 01:21:50 +0200

    Test about the doesFileExist

--- test/src/ToolsTests.cpp
+++ test/src/ToolsTests.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-#include <cmath>
-#include <vector>
-#include <bitset>
-#include <fstream>
-#include <streambuf>
-#include <algorithm>
-#include <sstream>
-#include <Tools/Tools.h>
+#include <string>
+#include <Tanja84dk/Tools/Tools.h>
 
 int main()
 {
-    printf("Test about writing a Binary Vector to a text file");
-    std::vector<std::bitset<8>> binaryVector = { 01001000, 01100101, 01101100, 01101100, 01101111, 00100000, 01010111, 01101111, 01110010, 01101100, 01100100};
-    Tools::writeFile(binaryVector, "testfile.txt");
-
-    Tools::printFile("testfile.txt");
+    bool r = false;
+    std::string filenametest = "";
+    std::cout << r << std::endl;
+    printf("Enter a file there exist: ");
+    std::getline(std::cin >> std::ws, filenametest);
+    r = Tanja84dk::Tools::doesFileExist(filenametest);
+    if (r == true)
+    {
+        printf("%s Exist", filenametest.c_str());
+    }
     return EXIT_SUCCESS;
 }
